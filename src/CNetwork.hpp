27652489bb733c1cd034bfe52bedba03e4a931ac
Alex Martin <maddin4t0r@gmail.com>
2016-11-29 13:15:31 +0100

    bug-fix: enum class is not hashable by default

--- src/CNetwork.hpp
+++ src/CNetwork.hpp
@@ -4,7 +4,7 @@
 #include "types.hpp"
 
 #include <string>
-#include <unordered_map>
+#include <map>
 #include <functional>
 #include <memory>
 #include <thread>
@@ -90,7 +90,7 @@ private: // variables
 	std::string m_SessionId;
 	asio::steady_timer m_HeartbeatTimer{ m_IoService };
 	std::chrono::steady_clock::duration m_HeartbeatInterval;
-	std::unordered_multimap<WsEvent, WsEventCallback_t> m_EventMap;
+	std::multimap<WsEvent, WsEventCallback_t> m_EventMap;
 
 private: // functions
 	bool WsConnect();
